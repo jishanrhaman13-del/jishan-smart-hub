a8471b35b62bda267c4ebd182e34e9e9e635eceb
jishanrhaman13-del <jishanrhaman13@gmail.com>
2026-08-04 00:35:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ document.addEventListener("DOMContentLoaded", () => {
 console.log("Premium Header Loaded");
 console.log("Categories Section Loaded");
 console.log("Featured Products Loaded");
+let h=12,m=59,s=59;
+
+setInterval(()=>{
+
+if(s>0){
+s--;
+}else{
+s=59;
+
+if(m>0){
+m--;
+}else{
+m=59;
+
+if(h>0){
+h--;
+}
+}
+}
+
+document.getElementById("hours").textContent=String(h).padStart(2,"0");
+document.getElementById("minutes").textContent=String(m).padStart(2,"0");
+document.getElementById("seconds").textContent=String(s).padStart(2,"0");
+
+},1000);
