12ca161051320d10f9dd52b6e368b14ddcd10198
jishanrhaman13-del <jishanrhaman13@gmail.com>
2026-08-03 22:59:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Jishan Mart Loaded Successfully 🚀");
+});window.addEventListener("load",()=>{
+console.log("Jishan Mart Premium Homepage Loaded");
 });
